4cd3b501b2ad95c72e747e17d1572a21f0f9aaa4
vitvlkv <vitvlkv@gmail.com>
2021-09-25 21:36:29 +0300

    More changes

--- keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
+++ keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
@@ -9,6 +9,8 @@ enum layers {
 #define MT_LALT LALT_T(KC_ESC)
 #define MT_RALT RALT_T(KC_ENT)
 #define MT_SPC LT(_RAISE, KC_SPC)
+#define MT_LSFT LSFT_T(KC_EQL)
+#define MT_RSFT RSFT_T(KC_MINS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
@@ -16,21 +18,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         XXXXXXX, KC_BSLS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX,
                  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_INS,  KC_PSCR,  KC_RBRC,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                                            KC__MUTE,XXXXXXX, KC_LSFT, MT_SPC,  MT_LALT, MT_RALT,  MO(_LOWER),KC_RSFT, XXXXXXX, KC_LGUI
+                                            KC__MUTE,KC_LGUI, MT_LSFT, MT_SPC,  MT_LALT, MT_RALT,  MO(_LOWER),MT_RSFT, KC_LGUI, XXXXXXX
     ),
     [_LOWER] = LAYOUT(
-                 XXXXXXX, XXXXXXX, XXXXXXX, KC_PLUS, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, KC_LBRC, KC_EQL,  KC_RBRC, XXXXXXX,                                     XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX,
-        XXXXXXX, _______, XXXXXXX, KC_LPRN, KC_MINS, KC_RPRN, KC_DEL,                                      KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, KC_LCBR, KC_UNDS, KC_RCBR, XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_PGUP, KC_APP,  KC_PGDN, XXXXXXX, _______,
-                                            _______, XXXXXXX, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+                 KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                                     KC_AMPR, KC_ASTR, KC_MINS, KC_UNDS, KC_PLUS, KC_EQL,
+                 _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_PCMM,                                     XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX,
+        XXXXXXX, _______, KC_0,    KC_4,    KC_5,    KC_6,    KC_DEL,                                      KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+                 _______, KC_0,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, _______, XXXXXXX, KC_PGUP, KC_APP,  KC_PGDN, XXXXXXX, _______,
+                                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
-                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, KC_PCMM, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX,                                     KC_0,    KC_1,    KC_2,    KC_3,    KC_0,    XXXXXXX,
-        XXXXXXX, _______, XXXXXXX, KC_DLR,  KC_PERC, KC_CIRC, KC_DEL,                                      KC_BSPC, KC_4,    KC_5,    KC_6,    KC_0,    XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, KC_AMPR, KC_ASTR, KC_PCMM, XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_0,    _______,
-                                            _______, XXXXXXX, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+                 KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                                     KC_AMPR, KC_ASTR, KC_MINS, KC_UNDS, KC_PLUS, KC_EQL,
+                 _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                                     XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX, XXXXXXX,
+        XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_DEL,                                      KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, XXXXXXX, XXXXXXX, XXXXXXX,
+                 _______, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_AMPR, KC_ASTR, KC_PCMM, XXXXXXX, _______,
+                                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
 
