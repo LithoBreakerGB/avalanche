0072d3c3ca4f4eed9f1350a6314f6fe99a874208
LithoBreakerGB <114358174+LithoBreakerGB@users.noreply.github.com>
2023-01-11 14:56:27 -0800

    Update keymap.c

--- keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
+++ keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
@@ -59,7 +59,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         if (clockwise) {
             tap_code(KC_MS_WH_UP);
         } else {
-            tap_code(KC_PGDOWN);
+            tap_code(KC_MS_WH_DOWN);
         }
     }
     return true;
