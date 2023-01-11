bc35799469efdc01c6b1318cb24437fe41f39a8c
LithoBreakerGB <114358174+LithoBreakerGB@users.noreply.github.com>
2023-01-10 16:11:15 -0800

    Update keymap.c

--- keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
+++ keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
+    _BASE,
     _LOWER,
     _RAISE,
 };
@@ -13,26 +13,25 @@ enum layers {
 #define MT_RSFT RSFT_T(KC_MINS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT(
-                 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                 KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-        XXXXXXX, KC_BSLS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX,
-                 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_INS,  KC_PSCR,  KC_RBRC,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                                            KC__MUTE,KC_LGUI, MT_LSFT, MT_SPC,  MT_LALT, MT_RALT,  MO(_LOWER),MT_RSFT, KC_LGUI, XXXXXXX
+    [_BASE] = LAYOUT(
+                    KC_1, KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                                           KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,
+                  KC_TAB, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                                           KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_CAPS,
+        XXXXXXX, KC_BSLS, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                                           KC_H,    KC_N,    KC_E,    KC_I,       KC_O,    KC_QUOT, KC_ENT,
+                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_INS,  KC_PSCR,  KC_RBRC,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_BSLS,
+                                            KC_LCTL,KC_LGUI, KC_ENT, TO(2),  KC_LALT, KC_RALT,  TO(1),KC_SPC, KC_LGUI, KC_RCTL
     ),
     [_LOWER] = LAYOUT(
-                 KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                                     KC_AMPR, KC_ASTR, KC_MINS, KC_UNDS, KC_PLUS, KC_EQL,
-                 _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_PCMM,                                     XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX,
-        XXXXXXX, _______, KC_0,    KC_4,    KC_5,    KC_6,    KC_DEL,                                      KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, KC_0,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, _______, XXXXXXX, KC_PGUP, KC_APP,  KC_PGDN, XXXXXXX, _______,
-                                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
+                    KC_1,    KC_2,       KC_3,      KC_4,      KC_5,       KC_6,                                         KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,
+                    KC_1,    KC_2,       KC_3,      KC_4,      KC_5,       KC_6,                                         KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,
+        XXXXXXX, KC_BSLS, KC_LCTL,    KC_LALT,    KC_ESC,    KC_DEL,    XXXXXXX,                                        KC_UP,  KC_GRV,  KC_LBRC, KC_RBRC,   KC_SCLN,    KC_QUOT, XXXXXXX,
+                 KC_LSFT,    KC_Z,       KC_X,      KC_C,      KC_V,    KC_LEFT,    _______, _______, _______, _______, KC_DOWN, KC_RGHT,  KC_COMM,  KC_DOT, KC_SLSH,    KC_BSLS,
+                                            KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(2),KC_SPC, KC_LGUI, KC_RCTL
     [_RAISE] = LAYOUT(
                  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                                     KC_AMPR, KC_ASTR, KC_MINS, KC_UNDS, KC_PLUS, KC_EQL,
-                 _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                                     XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX, XXXXXXX,
-        XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_DEL,                                      KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_AMPR, KC_ASTR, KC_PCMM, XXXXXXX, _______,
-                                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                 KC_BSLS, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                                     XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_BSLS, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_DEL,                                      KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, XXXXXXX, XXXXXXX, XXXXXXX,
+                  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_AMPR, KC_ASTR, KC_PCMM, XXXXXXX, _______,
+                                            KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(0),KC_SPC, KC_LGUI, KC_RCTL
     ),
 };
 
@@ -45,10 +44,35 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) { /* Second encoder */
         if (clockwise) {
-            tap_code(KC_PGUP);
+            tap_code(KC_MS_WH_UP);
         } else {
             tap_code(KC_PGDOWN);
         }
     }
     return true;
 }
+
+#void dip_switch_update_user(uint8_t index, bool active) 
+#{ 
+# switch (index) {
+#        case 0:		//ENC0_SW
+#        if(active) //ENC0 pressed3
+#		{ 
+#			tap_code(KC_MUTE);
+#		} 
+#	else //ENC0 released
+#               {
+#			//do nothing
+#		}
+
+# switch (index) {
+#        case 1:		//ENC1_SW
+#        if(active) //ENC1 pressed3
+#		{ 
+#			tap_code(KC_MUTE);
+#		} 
+#	else //ENC0 released
+#               {
+#			//do nothing
+#		}
+#            break;
