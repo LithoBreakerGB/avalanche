ac5774926b6a4ef06b4071ae12db8e96d45400d1
LithoBreakerGB <114358174+LithoBreakerGB@users.noreply.github.com>
2023-01-24 16:19:09 -0800

    Update keymap.c
    Fixed a few wrong keys and changed the screen

--- keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
+++ keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
@@ -26,30 +26,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
                 KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,                                         KC_7,    KC_8,     KC_9,    KC_0, KC_MINS,  KC_EQL,
               KC_TAB,    KC_Q,    KC_W,   KC_F,   KC_P,    KC_G,                                         KC_J,    KC_L,     KC_U,    KC_Y, KC_SCLN, KC_CAPS,
-    KC_LCTL, KC_BSLS,    KC_A,    KC_R,   KC_S,   KC_T,    KC_D,                                         KC_H,    KC_N,     KC_E,    KC_I,    KC_O, KC_QUOT, KC_ENT,
-             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V,    KC_B,    KC_GRV, KC_INS, KC_PSCR, KC_RBRC,    KC_K,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-                                             KC_LCTL, KC_LGUI, KC_ENT, TO(2), KC_LALT, KC_RALT, TO(1),  LT(1, KC_SPC), KC_LGUI, KC_RCTL
+    KC_LEFT_CTRL, KC_BSPC,    KC_A,    KC_R,   KC_S,   KC_T,    KC_D,                                         KC_H,    KC_N,     KC_E,    KC_I,    KC_O, KC_QUOT, KC_ENT,
+             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V,    KC_B,    KC_COPY, KC_INS, KC_PSCR, KC_PASTE,    KC_K,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+                                             KC_LCTL, KC_LGUI, KC_ENT, TO(2), KC_LALT, KC_RALT, TO(1), LT(1,KC_SPACE), KC_LGUI, KC_RCTL
     ),
     [_LOWER] = LAYOUT(
                 KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,                                          KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
                 KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,                                          KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
-    KC_LCTL, KC_BSLS, KC_LCTL, KC_LALT, KC_ESC, KC_DEL, XXXXXXX,                                         KC_UP,  KC_GRV, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, XXXXXXX,
-             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V, KC_LEFT,   KC_GRV, KC_INS, KC_PSCR, KC_RBRC,   KC_DOWN, KC_RGHT, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-                                             KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(2),KC_SPC, KC_LGUI, KC_RCTL
+    KC_LEFT_CTRL, KC_BSPC, KC_LCTL, KC_LALT, KC_ESC, KC_DEL, XXXXXXX,                                         KC_UP,  KC_GRV, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, XXXXXXX,
+             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V, KC_LEFT,   KC_COPY, KC_INS, KC_PSCR, KC_PASTE,   KC_DOWN, KC_RGHT, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
+                                             KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(2),KC_SPACE, KC_LGUI, KC_RCTL
     ),
     [_RAISE] = LAYOUT(
                 KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,                                          KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
                 KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,                                          KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
-    KC_LCTL, KC_BSLS, KC_LCTL, KC_LALT, KC_ESC, KC_DEL, XXXXXXX,                                         KC_UP, XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME,  KC_DEL, XXXXXXX,
-             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V, KC_LEFT,   KC_GRV, KC_INS, KC_PSCR, KC_RBRC,   KC_DOWN, KC_RGHT, XXXXXXX, KC_PGDN,  KC_END, XXXXXXX,
-                                            KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(3),KC_SPC, KC_LGUI, KC_RCTL
+    KC_LEFT_CTRL, KC_BSPC, KC_LCTL, KC_LALT, KC_ESC, KC_DEL, XXXXXXX,                                         KC_UP, XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME,  KC_DEL, XXXXXXX,
+             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V, KC_LEFT,   KC_COPY, KC_INS, KC_PSCR, KC_PASTE,   KC_DOWN, KC_RGHT, XXXXXXX, KC_PGDN,  KC_END, XXXXXXX,
+                                            KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(3),KC_SPACE, KC_LGUI, KC_RCTL
     ),    
     [_ADJUST] = LAYOUT(
                KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,   KC_F6,                                         KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
                KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,   KC_F6,                                         KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
-    XXXXXXX, KC_BSLS, KC_LCTL, KC_LALT, KC_ESC, KC_DEL, XXXXXXX,                                         KC_UP, XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME,  KC_DEL, XXXXXXX,
-             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V, KC_LEFT,   KC_GRV, KC_INS, KC_PSCR, KC_RBRC,   KC_DOWN, KC_RGHT, XXXXXXX, KC_PGDN,  KC_END, XXXXXXX,
-                                            KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(0),KC_SPC, KC_LGUI, KC_RCTL             
+    KC_LEFT_CTRL, KC_BSPC, KC_LCTL, KC_LALT, KC_ESC, KC_DEL, XXXXXXX,                                         KC_UP, XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME,  KC_DEL, XXXXXXX,
+             KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V, KC_LEFT,   KC_COPY, KC_INS, KC_PSCR, KC_PASTE,   KC_DOWN, KC_RGHT, XXXXXXX, KC_PGDN,  KC_END, XXXXXXX,
+                                            KC_LCTL,KC_LGUI, KC_ENT, TO(0),  KC_LALT, KC_RALT,  TO(0),KC_SPACE, KC_LGUI, KC_RCTL             
     ),
 };
 
@@ -96,4 +96,37 @@ void dip_switch_update_user(uint8_t index, bool active)
 			}
 		}
 }
+	
+	
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("COLEMAK\n"), false);
+            break;
+        case _LOWER:
+            oled_write_P(PSTR("SYMBOLS\nNUMBERS"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("FUNCTION\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("LAYER 4\n"), false);
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
 	
